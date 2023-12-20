4879ba58c1c08a3cc9eda9793dffa6282a02a314
Aya-Alsakkaf <aya.alsakkaf@ccse.ku.edu.kw>
2023-12-20 18:34:12 +0300

    Solved

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { BookStoreImg } from './BookStoreImg';
+import Card from './GreetingCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Card name="Scottish Bookstore" greeting="Where You Can Live a thousand lives"/>
+      <BookStoreImg height="" width="100%" />
+
     </div>
   );
 }
